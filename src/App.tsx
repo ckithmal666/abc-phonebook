17e54b2cf89ad4988ce84c5f62a7b13c947e0493
Chathura Kithmal <ckithmal666@gmail.com>
2023-04-22 21:03:06 +1000

    new icon and page headers

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { helpBuoy, addCircle, book } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -42,6 +42,9 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
+        <Route exact path="/loader">
+            <Loader />
+          </Route>
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
@@ -57,22 +60,20 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={book} />
+            <IonLabel>Contacts</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon aria-hidden="true" icon={addCircle} />
+            <IonLabel>Add</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={helpBuoy} />
+            <IonLabel>Help</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
-      <Route exact path="/loader">
-            <Loader />
-          </Route>
+      
     </IonReactRouter>
   </IonApp>
 );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,16 +7,16 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Add New Contact</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Add New Contact</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Add New Contact coming soon" />
       </IonContent>
     </IonPage>
   );
